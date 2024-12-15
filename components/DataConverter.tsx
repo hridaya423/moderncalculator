1f47b26e96c919dc06c9f83c91cd3fb381fdadd9
Hridya Agrawal <hridayahoney@gmail.com>
2024-12-15 16:23:29 +0000

    Update DataConverter.tsx

--- components/DataConverter.tsx
+++ components/DataConverter.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { TabsContent } from '@/components/ui/tabs';
